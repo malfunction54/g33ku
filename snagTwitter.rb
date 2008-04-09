422b9cc6eb878e82a2fcf41da5dddb0733cf656a
Malfunction54 <malfunction54@therac25.net>
2008-04-09 14:46:02 -0400

    basic code to grab twitter feed

---
+++ snagTwitter.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby
+
+require "rexml/document"
+require "net/http"
+
+include REXML
+
+rawTwitterXML = '';
+
+Net::HTTP.start('twitter.com', 80) { |http|
+    req = Net::HTTP::Get.new('/statuses/friends_timeline.xml');
+    req.basic_auth('malfunction54@therac25.net', 'rubyfruit911');
+    response = http.request(req);
+    rawTwitterXML = response.body;
+}
+
+doc = Document.new rawTwitterXML
+doc.root.elements.each("/statuses/status") { |status|
+    puts status.elements["user"].elements["name"].text \
+    + " : " + status.elements["text"].text;
+}
